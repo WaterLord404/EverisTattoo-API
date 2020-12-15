f1ccacf38ea0b7461f5d809200990cd7bd10874c
Salvador <alum.smunozc@iesalixar.org>
2020-12-15 14:52:45 +0100

    comentarios en service

--- src/main/java/com/everis/tattoo/service/ProductServiceI.java
+++ src/main/java/com/everis/tattoo/service/ProductServiceI.java
@@ -2,13 +2,38 @@ package com.everis.tattoo.service;
 
 import java.util.List;
 
+import com.everis.tattoo.model.entity.Product;
+
 public interface ProductServiceI {
 
+	/**
+	 * Returns a list of all products.
+	 * 
+	 * @return List<Product>
+	 */
 	public List<Product> getAllProducts();
-	
+
+	/**
+	 * Returns a product by Id.
+	 * 
+	 * @param id
+	 * @return Product
+	 */
 	public Product getProduct(final Long id);
-	
+
+	/**
+	 * Adds a product.
+	 * 
+	 * @param product
+	 * @return Product
+	 */
 	public Product addProduct(final Product product);
-	
+
+	/**
+	 * Deletes a product by Id.
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public Product deleteProductById(final Long id);
 }
